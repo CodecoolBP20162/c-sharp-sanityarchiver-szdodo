12a50791531bf368c37a22f20fcaa7d5c9123603
szdodo <szeghi.@gmail.com>
2017-06-26 15:04:20 +0200

    basic display prototype done

---
+++ SanityArchiver/DirectoryDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SanityArchiver
+{
+    class DirectoryDisplay
+    {
+        static string DefaultPath = "C:\\Users\\Dodo";
+
+
+
+        public DirectoryDisplay(ListBox lb)
+        {
+            DirectoryInfo rootDir = new DirectoryInfo(DefaultPath);
+            if (!rootDir.Exists)
+            {
+                lb.Items.Add("nincs ilyen");
+            }
+            else
+            {
+                lb.Items.Add("van ilyen");
+            }
+
+            foreach (DirectoryInfo fil in rootDir.GetDirectories())
+            {
+                lb.Items.Add(fil.ToString());
+            }
+
+        }
+    }
+}
---
+++ SanityArchiver/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SanityArchiver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var item = new TreeViewItem();
+            item.Header = "kak";
+            LibraryView.Items.Add(item);
+            var item2 = new TreeViewItem();
+            item2.Header = "kk";
+            LibraryView.Items.Add(item2);
+            var item3 = new TreeViewItem();
+            item3.Header = "kaaak";
+            item.Items.Add(item3);
+            DirectoryDisplay display = new DirectoryDisplay(lb);
+        }
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //find a file from the texbox
+            //show a default structure on open
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
